0c7094f89056c514c628079878962bfd5c000b27
emon2021 <mdemonahmed2021@gmail.com>
2023-09-29 00:09:51 +0600

    Migration

---
+++ database/migrations/2023_09_28_170707_create_students_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('class_id');
+            $table->string('name',50)->nullable();
+            $table->integer('roll')->nullable();
+            $table->integer('phone')->nullable();
+            $table->string('email',255)->nullable();
+            $table->timestamps();
+            $table->foreign('class_id')->references('id')->on('classes')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+};
